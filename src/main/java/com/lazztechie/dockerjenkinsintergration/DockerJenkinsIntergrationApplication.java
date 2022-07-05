ed5f5006bcff064eb3aa475b9dd9787524007a09
LAZKORIR <lazaruskorir95@gmail.com>
2022-07-05 18:07:49 +0300

    added java 8 features

--- src/main/java/com/lazztechie/dockerjenkinsintergration/DockerJenkinsIntergrationApplication.java
+++ src/main/java/com/lazztechie/dockerjenkinsintergration/DockerJenkinsIntergrationApplication.java
@@ -12,16 +12,17 @@ public class DockerJenkinsIntergrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntergrationApplication.class, args);
 		IntegrationServiceImpl integrationService = new IntegrationServiceImpl();
-//		integrationService.call();
-//		integrationService.message();
-//		integrationService.lups();
-//
-//		LambdaDemoService lambdaDemoService;
-//		lambdaDemoService= i -> System.out.println("lambda demo  number = "+i);
-//
-//		lambdaDemoService.lambdaDemo(5);
+		integrationService.call();
+		integrationService.message();
+		integrationService.lups();
+
+		LambdaDemoService lambdaDemoService;
+		lambdaDemoService= i -> System.out.println("lambda demo  number = "+i);
+
+		lambdaDemoService.lambdaDemo(5);
 
 		integrationService.dateTimeDemo();
+		integrationService.streamsDemo();
 	}
 
 }
--- src/main/java/com/lazztechie/dockerjenkinsintergration/service/IntegrationService.java
+++ src/main/java/com/lazztechie/dockerjenkinsintergration/service/IntegrationService.java
@@ -3,6 +3,9 @@ package com.lazztechie.dockerjenkinsintergration.service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public interface IntegrationService {
     void call();
@@ -19,5 +22,28 @@ public interface IntegrationService {
         System.out.println("date =="+date);
         System.out.println("time =="+time);
         System.out.println("dateTime =="+dateTime);
+    }
+
+    default void streamsDemo(){
+       String[] names ={"laz","Angie","laban","Kim","Kaka"};
+        Arrays.stream(names)//same as Streams.of(names)
+                .filter(x -> x.startsWith("l"))
+                .sorted()
+                .forEach(System.out::println);
+
+        int[] numbers={2,5,6};
+        Arrays.stream(numbers)
+                .map(x-> x * x)
+                .average()
+                .ifPresent(System.out::println);
+
+        List<String> people = Arrays.asList("LAzz","ANGIE","LABAN","KABUlio");
+        people
+                .stream()
+                .map(String::toLowerCase)
+                .filter(x -> x.startsWith("l"))
+                .sorted()
+                .forEach(System.out::println);
+
     }
 }
